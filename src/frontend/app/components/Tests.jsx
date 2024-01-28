ffe48d03e55ec30f63594f576f3fa4bd1b52a3c9
Peper20 <ivan.baluk@bk.ru>
2024-01-28 22:22:36 +0200

    update tests cards

--- src/frontend/app/components/Tests.jsx
+++ src/frontend/app/components/Tests.jsx
@@ -3,21 +3,44 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 
+import last_visit_logo from '../assets/imgs/last_visit_logo.png'
+import created_logo from '../assets/imgs/created_logo.png'
+
+
 
 export default function Tests(){
     return (
         <Row xs={1} md={2} lg={3} xl={4} xxl={5} className="g-4">
         {Array.from({ length: 5 }).map((_, idx) => (
           <Col key={idx}>
-            <Card>
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
+            <Card className='shadow-sm'>
+              <Card.Body className='p-3'>
+                <Card.Title>Название теста</Card.Title>
+                {idx % 2
+                  ?<Card.Subtitle className="mb-2 text-danger">Закрыт</Card.Subtitle>
+                  :<Card.Subtitle className="mb-2 text-success">Открыт</Card.Subtitle>
+                }
+                {/* <Card.Text>
                   This is a longer card with supporting text below as a natural
                   lead-in to additional content. This content is a little bit
                   longer.
-                </Card.Text>
-              </Card.Body>
+                </Card.Text> */}
+            </Card.Body>
+            <Card.Footer className='p-2'>
+                <small className="text-muted">
+                    <img alt="Просмотрено" src={last_visit_logo} style={{
+                        width: '1.1em', height: '1.1em',
+                        marginRight: '0.3em', marginLeft: '0px'
+                    }} className='mb-1'/>
+                    25.01.2024 17:23
+                    <br />
+                    <img alt="Создано" src={created_logo} style={{
+                        width: '1.3em', height: '1.3em',
+                        marginRight: '0.1em',
+                    }} className='mb-1'/>
+                    23.01.2024 12:43
+                </small>
+            </Card.Footer>
             </Card>
           </Col>
         ))}
